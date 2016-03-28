a6e291c4ab5c2d0bd3705cd033b088101a406b8c
ChunyangLiu233 <411374819@qq.com>
2016-03-28 15:57:00 +0800

    9

---
+++ app/js/shoppingCartService.js
@@ -0,0 +1,12 @@
+/**
+ * Created by JINYANG么么 on 2016/3/28.
+ */
+'use strict'
+var shoppingCartServices = angular.module('shoppingCartServices',['ngResource']);
+shoppingCartServices.factory('shoppingCartService',function($resource){
+       return $resource('/shoppingcart/:id', { id: '@_id' }, {
+               update: {
+                   method: 'PUT' // this method issues a PUT request
+                }});
+
+        })
